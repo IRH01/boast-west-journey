7b22f491d1368be22f71dfb6201b088a60c0ac48
iritchie.ren <Renh1298@13322.com>
2017-09-29 11:14:34 +0800

    整理

--- src/main/java/com/hhly/smartdata/websocket/WebSocketDemo.java
+++ src/main/java/com/hhly/smartdata/websocket/WebSocketDemo.java
@@ -10,13 +10,13 @@ import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 @ServerEndpoint(value = "/websocket")
-public class MyWebSocket {
+public class WebSocketDemo {
 
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。  
     private static int onlineCount = 0;
 
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识  
-    private static CopyOnWriteArraySet<MyWebSocket> webSocketSet = new CopyOnWriteArraySet<MyWebSocket>();
+    private static CopyOnWriteArraySet<WebSocketDemo> webSocketSet = new CopyOnWriteArraySet<>();
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据  
     private Session session;
@@ -29,8 +29,8 @@ public class MyWebSocket {
     @OnOpen
     public void onOpen(Session session) {
         this.session = session;
-        webSocketSet.add(this);     //加入set中  
-        addOnlineCount();           //在线数加1  
+        webSocketSet.add(this);     //加入set中
+        addOnlineCount();           //在线数加1
         System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
     }
 
@@ -55,7 +55,7 @@ public class MyWebSocket {
         System.out.println("来自客户端的消息:" + message);
 
         //群发消息  
-        for (MyWebSocket item : webSocketSet) {
+        for (WebSocketDemo item : webSocketSet) {
             try {
                 item.sendMessage(message);
             } catch (IOException e) {
@@ -93,11 +93,11 @@ public class MyWebSocket {
     }
 
     public static synchronized void addOnlineCount() {
-        MyWebSocket.onlineCount++;
+        WebSocketDemo.onlineCount++;
     }
 
     public static synchronized void subOnlineCount() {
-        MyWebSocket.onlineCount--;
+        WebSocketDemo.onlineCount--;
     }
 
 }  
